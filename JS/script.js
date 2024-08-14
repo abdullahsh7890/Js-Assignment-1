92af244f09f86cdcbcb06f3cd1c343b38c6f2281
Abdullah Bin Shahbaz <154223491+abdullahsh7890@users.noreply.github.com>
2024-08-14 17:48:46 +0500

    Add files via upload

---
+++ JS/script.js
@@ -0,0 +1,138 @@
+// // Q1
+//  alert("Assalam o alaikum")
+// // Q2
+// alert("Error! please Enter a valid password")
+// // Q3
+// alert("Welcome to \n Bano Qabil")
+// // Q4
+// alert("Hello")
+// alert("In")
+// alert("Programming World")
+// // Q7
+// var username;
+// // Q8
+// username= "Abdullah Bin Shahbaz"
+// console.log(username)
+// // Q9
+// var message;
+// message="Hello World"
+// alert(message)
+// // Q10
+// var nam = "Name: Abdullah"
+// var cla= "Class: 12"
+// var age = "Age: 18"
+// var bioData = nam+ "\n" + cla + "\n" + age
+// alert(bioData)
+// // Q11
+// var email= "abdullahshahbaz123@gmail.com"
+// alert(email)
+// // Q13
+// var emoj = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬"
+// alert(emoj)
+// // Q14
+// var age;
+// age=18
+// alert(age)
+// // Q16
+// var birthYear;
+// birthYear=2007
+// alert(birthYear)
+// Q17
+var visitorName="John Doe "
+var product= "Jeans "
+var quantity = 7  
+console.log(visitorName + "ordered " + quantity + " " +product+ "on XYZ Clothing Store")
+// Q18
+var x = 1,y=2,z=3
+// Q19
+//Legal Variables
+var userAge;
+var totalPrice;
+var isActive;
+var maxValue;
+var dataList;
+//ILLEGAL Variables
+// var 'userAge';
+// var 1totalPrice;
+// var first name;
+// var class;
+// var @userAge;
+
+// Q20
+//a
+document.write("<h1>Rules for naming JS variable </h1>")
+
+//b
+
+document.write("Variable names can only contain Digits, Letters,Underscore and Dollar signs. For example $my_1stVariable ")
+//c
+document.write("Variables must begin with a Digits,Letters or Dollar signs. For example$name, _name or name")
+//d
+document.write("Variable names are case Sensitive")
+// e
+document.write("Variable names should not be JS Reserved Words")
+
+//Q21
+var num1=+prompt("Enter a First Number for add")
+var num2 = +prompt("Enter a Second Number for add")
+var result = console.log(num1 + num2)
+// Q22
+var num3=+prompt("Enter a First Number for multipliction")
+var num4 = +prompt("Enter a Second Number for multiplication")
+var result1 = console.log(num1*num2)
+
+var num5=+prompt("Enter a First Number for sub")
+var num6 =+prompt("Enter a Second Number for sub")
+var result2 = console.log(num1-num2)
+
+var num7=+prompt("Enter a First Number for divide")
+var num8 =+prompt("Enter a Second Number for divide")
+var result3 = console.log(num1/num2)
+// Q23
+// a 
+var a;
+//b
+a=5
+console.log(a) // variable after declaration
+//c
+a=5;
+//d
+console.log(a)
+//e
+a++
+//f
+console.log(a)
+//g
+var b= a+7;
+//h
+console.log(b)
+//i
+b--
+//j
+console.log(b)
+//k
+var c = b%3;
+//l
+console.log(c)
+
+// Q24
+var ticket = 600
+var costOfFive = 600*5
+console.log(costOfFive)
+
+// Q25
+// prompt("Enter a number")
+// document.write("<br/><br/><br/>Table of 5")
+// var a = 5
+
+// document.write("<br/>5x1 =" + a*1)
+// document.write("<br/>5x2 =" + a*2)
+// document.write("<br/>5x3 =" + a*3)
+// document.write("<br/>5x4 =" + a*4)
+// document.write("<br/>5x5 =" + a*5)
+// document.write("<br/>5x6 =" + a*6)
+// document.write("<br/>5x7 =" + a*7)
+// document.write("<br/>5x8 =" + a*8)
+// document.write("<br/>5x9 =" + a*9)
+// document.write("<br/>5x10=" + a*10)
+
